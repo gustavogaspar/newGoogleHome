fa108fa14add512815c3e3e7cedf757ba41ebef7
gustavogaspar <gaspar.gualves@gmail.com>
2019-03-01 18:32:06 -0300

    teste com exemplo do google heroku/fullfilment

--- service.js
+++ service.js
@@ -4,10 +4,10 @@ const app = dialogflow();
 
 module.exports = (serviceConfig) => {
 
-app.intent('Default Fallback Intent',conv =>{
-    conv.ask('Funcionou');
-});
+    app.intent('Default Fallback Intent', conv => {
+        conv.ask(`Funciona sua porra veia`)
+      })
 
-serviceConfig.use('bot/action',bodyParser.json(),app);
-serviceConfig.post('bot/action',app);
+serviceConfig.use('/fulfillment',bodyParser.json(),app);
+serviceConfig.post('/fulfillment',app);
 }
