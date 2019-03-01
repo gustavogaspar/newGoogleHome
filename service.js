7f0c13ab0065a45efbd584632895b19b8711d930
gustavogaspar <gaspar.gualves@gmail.com>
2019-03-01 20:21:23 -0300

    Full implementation

--- service.js
+++ service.js
@@ -5,12 +5,9 @@ const bodyParser = require('body-parser')
 const app = dialogflow()
 const log = console
 
-//Server start
 module.exports = (serviceConfig) => {
-    //ODA initialization
     OracleBot.init(serviceConfig)
 
-     //Webhook Configuration
     const webhook = new WebhookClient({
         channel: {
             url:'https://botv2iad1I0100H203896bots-mpaasocimt.botmxp.ocp.oraclecloud.com:443/connectors/v1/tenants/idcs-100b89d671b54afca3069fe360e4bad4/listeners/webhook/channels/d0743e8a-c3f4-4bc8-8b80-bd80794c7a58',
@@ -18,43 +15,43 @@ module.exports = (serviceConfig) => {
         }
     })
 
-        //Event Handlers
     webhook
         .on(WebhookEvent.ERROR, err => log.error('Error:',err.message))
         .on(WebhookEvent.MESSAGE_RECEIVED, message => log.info('Message to Google:',message))
         .on(WebhookEvent.MESSAGE_SENT, message => log.info('Message from Google:', message))
     
-    //Step that gets the Google message, parse into ODA structure, and sends to ODA
-    //Command that will get the text from google
-    app.intent('Default Fallback Intent', conv => {
-        //Command that calls the function to parse the message and sends to ODA
-        webhook.send(messageParseToODA(conv.query))         
-      })
-    //Function that parses the message
-    function messageParseToODA(text)
-    {
-        if(!text)
-        {
-            console.log('The message wasnt parsed correctly')
-            conv.close('The message wasnt parsed correctly')
-        }
-        else
-        {
-        const messageModel=webhook.MessageModel()
-        const message= {
-            userId: "Google Home",
-            messagePayload: messageModel.textConversationMessage(text)
-            }
-        return message
-        }
-    }
 
-    //Step that gets the message from ODA and passes to Google
-    webhook.on(WebhookEvent.MESSAGE_RECEIVED, message =>{
-        conv.ask(message)
-    })
+    app.intent('Default Fallback Intent', conv => {
+        const promise = new Promise(function(resolve,reject){
+             webhook.send(messageParseToODA(conv.query))
+             webhook.on(WebhookEvent.MESSAGE_RECEIVED, message => resolve(message))
+             function messageParseToODA(text)
+            {
+                if(!text)
+                {
+                    console.log('The message wasnt parsed correctly')
+                    conv.close('The message wasnt parsed correctly')
+                }
+                else
+                {
+                const messageModel=webhook.MessageModel()
+                const message= {
+                    userId: "Google Home",
+                    messagePayload: messageModel.textConversationMessage(text)
+                    }
+                return message
+                }
+            }
+        })
+        .then(function (result){
+            conv.ask(result.messagePayload.text)
+        })
+        return promise;
+      })
+    
+
 //Oracle Chatbot Endpoint
-app.post('/bot/message', webhook.receiver());
+serviceConfig.post('/bot/message', webhook.receiver());
 //Google Post Endpoint
 serviceConfig.use('/fulfillment',bodyParser.json(),app);
 serviceConfig.post('/fulfillment',app);
